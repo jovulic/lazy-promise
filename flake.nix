f0419140860be219ad55c7691138ca4747b83c90
Josip Vulic <jovulic@gmail.com>
2025-02-05 18:23:55 -0500

    add: setup nix flake

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Lazy Promise. A lazy promise that waits until you ask it to get to work.";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { ... }@inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        ctl =
+          with pkgs;
+          writeShellApplication {
+            name = "ctl";
+            text = with builtins; readFile ./ctl/ctl;
+          };
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = [
+            pkgs.git
+            pkgs.bash
+            pkgs.bashly
+            ctl
+            pkgs.nodejs
+            pkgs.nodePackages.npm
+          ];
+        };
+      }
+    );
+}
