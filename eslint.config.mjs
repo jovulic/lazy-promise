31e2f0c0c727609894f1964e34e7859b8d4058c4
Josip Vulic <jovulic@gmail.com>
2025-02-06 14:18:57 -0500

    upd: update build to write to dist

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,13 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
-  { ignores: [".output/**"] },
+  { ignores: ["dist/**"] },
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "warn"
-    }
-  }
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
 );
