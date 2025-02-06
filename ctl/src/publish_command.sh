2e80d5deebb92ebf3b11290e35e4739d06c6e7f8
Josip Vulic <jovulic@gmail.com>
2025-02-06 14:02:39 -0500

    upd: do not allow publish if there are uncommitted changes

--- ctl/src/publish_command.sh
+++ ctl/src/publish_command.sh
@@ -1,3 +1,9 @@
 # shellcheck shell=bash
 
+# We do not want to proceed with publishing if there are uncommitted changes.
+if [ -n "$(git status --porcelain)" ]; then
+  echo "unable to publish as there are uncommitted changes"
+  exit 1
+fi
+
 npm run publish
