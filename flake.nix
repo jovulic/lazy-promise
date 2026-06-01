ad74591f71b19984e4d50f902a7956312e34204b
Josip Vulic <jovulic@gmail.com>
2026-06-01 18:05:58 +1000

    ftr: update nixpkgs to 26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A lazy promise that waits until you ask it to get to work.";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.11";
+    nixpkgs.url = "nixpkgs/nixos-26.05";
   };
 
   outputs =
@@ -36,7 +36,6 @@
               pkgs.bash
               pkgs.just
               pkgs.nodejs
-              pkgs.nodePackages.npm
             ];
             shellHook = ''
               just setup lazy
